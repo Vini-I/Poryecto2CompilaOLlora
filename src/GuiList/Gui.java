0995763f564066c86750e40d338f0b8760a867c0
nloria26 <niloriaug@est.utn.cr>
2025-08-22 20:04:42 -0600

    Clase GuiList
    La clase Gui se crea con los métodos genéricos para la interfaz gráfica de usuario (GUI), tales como: clear, validateRequired, save, update, delete, search y showData

---
+++ src/GuiList/Gui.java
@@ -0,0 +1,21 @@
+
+package GuiList;
+
+public interface Gui {
+       
+    public void clear();
+        
+    public boolean validateRequiere();
+        
+    public void save();
+        
+    public void update();
+
+    public void delete();
+      
+    public void search();
+    
+    public void showData();
+    
+    
+}
