e876e1bb6f309cfeb137a790b9c8b7132770af57
Joches29 <jochaveshe@est.utn.ac.cr>
2025-08-22 20:16:16 -0600

    Clase vehiculos
    La clase que va a gestionar la informacion de vehiculos, todos sus atributos, metodos de acceso, metodo constructor y sus debidas validaciones de ingreso de datos.

---
+++ src/Vehiculos/Vehiculo.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehiculos;
+import Lists.List;
+import Utils.UtilDate;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author rodol
+ */
+public class Vehiculo {
+    private String plate;
+    private String brand;
+    private String model;
+    private LocalDate year;
+    private TipoVehiculo type;
+    private EstadoVehiculo state;
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public LocalDate getYear() {
+        return year;
+    }
+
+    public TipoVehiculo getType() {
+        return type;
+    }
+
+    public EstadoVehiculo getState() {
+        return state;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setType(TipoVehiculo type) {
+        this.type = type;
+    }
+
+    public void setState(EstadoVehiculo state) {
+        this.state = state;
+    }
+
+    public Vehiculo(String plate, String brand, String model, String year, TipoVehiculo type) {
+        if(plate.matches("^[\\w]{3}-[1-9]{3}$")){
+         this.plate = plate;   
+        }
+        this.brand = brand;
+        this.model = model;
+        if(UtilDate.isNotFutureDate(year) && UtilDate.isNotOlderThan(year)){
+         this.year = year;   
+        }
+        this.type = type;
+        this.state = EstadoVehiculo.AVAILABLE;
+    }
+    
+}
