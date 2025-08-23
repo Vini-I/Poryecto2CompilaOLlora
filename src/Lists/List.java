d85a4030193ceb8da7bf0e27aa3a2f68f1443931
nloria26 <niloriaug@est.utn.cr>
2025-08-22 19:52:42 -0600

    Clase List
    Se crea la clase List, interfaz generica con los metodos add, find y remove

---
+++ src/Lists/List.java
@@ -0,0 +1,16 @@
+
+package Lists;
+
+
+public interface List <T> { //Interfaz generica//
+    
+   
+    
+    public boolean add (T t);
+    
+    public T find(Object id);
+    
+    public boolean remove(T t);
+    
+   
+}
