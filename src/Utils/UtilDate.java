2318b07a21554f7358721c41280b5539a5866c8b
Joches29 <jochaveshe@est.utn.ac.cr>
2025-08-22 20:25:28 -0600

    Se añade un metodo
    Se crea el metodo para validar que la antiguedad de un vehiculo no sobrepase los 20 años.

--- src/Utils/UtilDate.java
+++ src/Utils/UtilDate.java
@@ -8,28 +8,28 @@ import java.time.format.DateTimeFormatter;
 
 public abstract class UtilDate {
 
-public static int calculateAge(LocalDate date){
- return Period.between(date, LocalDate.now()).getYears();   
-}    
-    
-public static boolean isNotFutureDate(LocalDate date){
-    return !date.isAfter(LocalDate.now());
-}
-    
-public static boolean isLegalAge(LocalDate date){
-    return calculateAge(date)>=18;
-}
-    
-public static LocalDate toLocalDate(String date){
-    return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-}
-    
-public static String toString(LocalDate date){
-    return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-}   
-
-    public static int calculateAge(int years) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public static int calculateAge(LocalDate date) {
+        return Period.between(date, LocalDate.now()).getYears();
     }
- 
+
+    public static boolean isNotFutureDate(LocalDate date) {
+        return !date.isAfter(LocalDate.now());
+    }
+
+    public static boolean isLegalAge(LocalDate date) {
+        return calculateAge(date) >= 18;
+    }
+
+    public static LocalDate toLocalDate(String date) {
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public static String toString(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public static boolean isNotOlderThan20(LocalDate date) {
+        return calculateAge(date)  <= 20;
+    }
+
 }
